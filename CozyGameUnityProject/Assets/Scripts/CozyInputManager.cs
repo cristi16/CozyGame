1895b53d39c64709a68e8239b473dbc2695cf188
Ted de Munnik <ta.demunnik@gmail.com>
2018-04-14 17:59:26 +0200

    First movement / pistol prototype scene

---
+++ CozyGameUnityProject/Assets/Scripts/CozyInputManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using XInputDotNetPure;
+
+public class CozyInputManager : MonoBehaviour {
+    public PlayerCharacterController player0;
+
+    void Update()
+    {
+        GamePadState state = GamePad.GetState(0, GamePadDeadZone.Circular);
+        player0.moveInput = new Vector2(state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y);
+        player0.isFiring = state.Triggers.Right > 0.5f;
+    }
+}
---
+++ CozyGameUnityProject/Assets/Scripts/PlayerCharacterController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    [HideInInspector] public Vector2 moveInput;
+    [HideInInspector] public bool isFiring;
+    public PistolWeapon currentWeapon;
+
+    private CharacterController m_CharacterController = null;
+    private bool m_LastIsFiring = false;
+
+    void Start()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        m_CharacterController.SimpleMove(new Vector3(moveInput.x, 0.0f, moveInput.y) * moveSpeed);
+
+        // Update weapon handling
+        if(currentWeapon != null)
+        {
+            if(m_LastIsFiring != isFiring)
+            {
+                if(isFiring)
+                {
+                    currentWeapon.Fire();
+                }
+                m_LastIsFiring = isFiring;
+            }
+        }
+    }
+}
