f89195e6f61f4018551c67f61bc32f39c9f3429f
strelles <strelles@gmail.com>
2018-04-14 19:19:32 +0200

    camera test 2

--- CozyGameUnityProject/Assets/CharacterMove.cs
+++ CozyGameUnityProject/Assets/CharacterMove.cs
@@ -6,6 +6,8 @@ public class CharacterMove : MonoBehaviour {
 
     public float speed = 10f;
 
+    public bool playerOne = true;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,21 +15,43 @@ public class CharacterMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (playerOne)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+            }
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else
         {
-            transform.Translate(Vector3.down * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
-        }
+            if (Input.GetKey(KeyCode.I))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.K))
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.J))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+            }
+        }    
     }
 }
--- CozyGameUnityProject/Assets/SmoothFollow.cs
+++ CozyGameUnityProject/Assets/SmoothFollow.cs
@@ -7,30 +7,67 @@ public class SmoothFollow : MonoBehaviour
     public List<Transform> targetList;
 
 
-    public float oSizeMin;
-    public float oSizeMax;
+    float oSizeMin=20f;
+	float oSizeMax=float.MaxValue;
+
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
 
     public void Update()
     {
         Vector3 center = Vector3.zero;
 
+        float maxX = float.MinValue;
+        float minX = float.MaxValue;
+        float maxY = float.MinValue;
+        float minY = float.MaxValue;
+
         foreach(Transform t in targetList)
         {
             center += t.position;
+
+            if (t.position.x < minX) minX = t.position.x;
+            if (t.position.x > maxX) maxX = t.position.x;
+
+            if (t.position.y < minY) minY = t.position.y;
+            if (t.position.y > maxY) maxY = t.position.y;
         }
 
         if (targetList.Count > 0) {
             center /= targetList.Count;
         }
 
-        center.z = -25f;
+        center.z = transform.position.z;
 
-        Vector3 translateVector = Vector3.ClampMagnitude(center - transform.position, 0.5f);
+        transform.position = Vector3.SmoothDamp(transform.position, center, ref velocity, smoothTime, 25f);
 
-        translateVector /= 2f;
+        //float ortographicSize = (targetList[0].position - center).magnitude/1.5f;
+        float deltaX = maxX - minX;
+        float deltaY = maxY - minY;
 
-        transform.Translate(translateVector);
-                
+		float ortographicFinal;
+
+		if(deltaX>deltaY){
+			ortographicFinal = Mathf.Sqrt(deltaX)*5f;
+		} else {
+			ortographicFinal = Mathf.Sqrt(deltaY)*5f;
+		}
+       
+		/*Debug.Log("X: "+ deltaX);
+        Debug.Log("Y: " + deltaY);
+        Debug.Log("");
+		*/
+
+        //float currentAspect = (float)Screen.width / (float)Screen.height;
+		ortographicFinal = (Screen.height) * (ortographicFinal / 800f);
+
+		ortographicFinal = Mathf.Clamp(ortographicFinal,oSizeMin,oSizeMax);
+		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, ortographicFinal, Time.deltaTime);        
+
+		Debug.Log ("Screen W" + Screen.width);
+		Debug.Log ("Screen H" + Screen.height);
+		Debug.Log ("Delta X" + deltaX);
+		Debug.Log ("Delta Y" + deltaY);
     }
 
 }
