1fabd5eb382bbaf13379aecca08e1d7394fe2a19
Toke Krainert <git@krainert.com>
2018-04-15 18:22:17 +0200

    LevelExit in camp level

---
+++ CozyGameUnityProject/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<PlayerCharacterController>() != null) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
