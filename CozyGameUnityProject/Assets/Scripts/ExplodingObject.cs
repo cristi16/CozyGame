cae73f2e9f94bb7540864c8c095867378ff9f84a
cristi16 <patrascu.cristinel@gmail.com>
2018-04-15 15:25:44 +0200

    explosions

---
+++ CozyGameUnityProject/Assets/Scripts/ExplodingObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingObject : MonoBehaviour, IBulletHitListener
+{
+    public int strength = 1;
+    public GameObject explosionPrefab;
+    public float explosionScale = 0.5f;
+
+    public void OnBulletHit(BulletHitInfo hit)
+    {
+        strength--;
+
+        if (strength == 0)
+        {
+            var go = GameObject.Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            go.transform.localScale = Vector3.one * explosionScale;
+
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
