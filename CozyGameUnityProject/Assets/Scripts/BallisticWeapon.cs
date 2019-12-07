61dc24264759d1189fa027e53e5323f3a38cf291
Toke Krainert <git@krainert.com>
2019-12-07 17:16:06 +0100

    Added scripts for new weapon system

---
+++ CozyGameUnityProject/Assets/Scripts/BallisticWeapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallisticWeapon : Weapon
+{
+    public GameObject Projectile;
+    public float FiringRate;
+    public float MuzzleDistance;
+    public int Ammunition;
+    public int MaxAmmunition;
+    public IController Controller = null;
+
+    private float lastShot;
+
+    void FixedUpdate() {
+        if (Controller != null && Controller.Fire && Time.fixedTime >= lastShot + 1f / FiringRate) {
+            if (Ammunition > 0) {
+                Instantiate(Projectile, transform.position + MuzzleDistance * Vector3.right, transform.rotation);
+                Ammunition--;
+            } else {
+                //TODO: Out-of-ammo click
+            }
+            lastShot = Time.fixedTime;
+        }
+    }
+
+    public interface IController {
+        bool Fire { get; }
+    }
+}
---
+++ CozyGameUnityProject/Assets/Scripts/WeaponInventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInventory : MonoBehaviour
+{
+    public Weapon Weapon;
+
+    void Awake() {
+        Weapon = GetComponentInChildren<Weapon>();
+    }
+
+    public void Equip(Weapon weapon) {
+        if (weapon != Weapon) {
+            if (Weapon != null) {
+                Weapon.transform.parent = transform.parent;
+            }
+            Weapon = weapon;
+            weapon.transform.parent = transform;
+            weapon.transform.localPosition = Vector3.zero;
+            weapon.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void Unequip() {
+        Weapon.transform.parent = transform.parent;
+        Weapon = null;
+    }
+}
